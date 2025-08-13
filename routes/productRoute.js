5a771f13291123e4e997612785e1a83ea3c80a81
meenu <meenubabu273@gmail.com>
2025-08-13 23:59:27 +0530

    changes applied

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,12 +1,12 @@
 import express from "express"
 import { createProduct, deleteProduct, getAllProducts, getProductById } from "../controllers/productController.js"
-
-
+import upload from "../middleware/multer.js"
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter=express.Router()
 
-productRouter.post('/create',createProduct)
-productRouter.post('/delete',deleteProduct)
+productRouter.post('/create',adminAuth,upload.single('image'),createProduct)
+productRouter.post('/delete',adminAuth,deleteProduct)
 productRouter.post('/getone',getProductById)
 productRouter.get('/getall',getAllProducts)
 
