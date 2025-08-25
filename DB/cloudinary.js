a434ef4b84c754d6b63282a9b9b5d09ec58de403
meenu <meenubabu273@gmail.com>
2025-08-25 23:06:45 +0530

    changes applied

--- DB/cloudinary.js
+++ DB/cloudinary.js
@@ -4,7 +4,7 @@ const connectCloudinary=async()=>{
     cloudinary.config({
         cloud_name:process.env.CLDN_NAME,
         api_key:process.env.CLDN_API_KEY,
-        api_secrect:process.env.CLDN_SECRET_KEY
+        api_secret:process.env.CLDN_API_SECRET
 
     })
 }
